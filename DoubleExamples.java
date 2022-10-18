3149fa42bf35901bbc2d0e26f2cedd00c4155b2b
smithb <smithb@wlwv.k12.or.us>
2022-10-18 08:21:27 -0700

    initial commit

---
+++ DoubleExamples.java
@@ -0,0 +1,35 @@
+// Integer and Double are in
+// java.lang package
+
+
+import java.lang.*;
+
+public class DoubleExamples
+{
+    public static void main(String[] args)
+    {
+        // Explictly create a Double
+        
+        
+        // Use autoboxing to have
+        // Java convert the double value
+        // to an Double object
+        
+        
+        // Can use Doubles exactly like
+        // you can use doubles because
+        // of autoboxing
+        
+        
+        // Can also use the doubleValue value
+        // to get the underlying double
+        
+        
+        
+        // One instance where unboxing doesn't work
+        // is when converting from a double to an int
+        
+        
+        
+    }
+}
---
+++ LiftsTester.java
@@ -0,0 +1,35 @@
+public class LiftsTester
+{
+    public static void main(String[] args)
+    {
+        
+        
+        // Create variables 
+        
+        
+        /* This method takes a Double (object).
+         * Note that both calls work. The
+         * first passes a Double (object) and
+         * the second passes a double (primitive).
+         */
+        
+        
+        
+        /* This method takes a double (primitive).
+         * Note that both calls work. The first
+         * passes a double (primitive) and the
+         * second passses a Double (object).
+         */
+        
+        
+        // This method takes an int, but
+        // accepts Integer
+        
+        
+        // This method takes an Integer, but
+        // accepts int
+        
+        
+        
+    }
+}
